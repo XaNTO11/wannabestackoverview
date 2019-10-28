84f7e557d7a3005718c76a06e49ebf6932bb6614
Brian <XaNTo1337@gmail.com>
2019-10-28 11:11:52 +0100

    Removed .env from gitignore

---
+++ client/build/precache-manifest.1a1fec52fdcfe8ca5d1cce1719fc6ad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19560380f1b255c1a8dd0ed770edaa71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df6f2001ccc86f73f651",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a68f3447bf422d6d8eca",
+    "url": "/static/js/2.c597d4e5.chunk.js"
+  },
+  {
+    "revision": "df6f2001ccc86f73f651",
+    "url": "/static/js/main.9a791ffe.chunk.js"
+  },
+  {
+    "revision": "3420f20996158c56a481",
+    "url": "/static/js/runtime-main.3bca8455.js"
+  }
+]);
