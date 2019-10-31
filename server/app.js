fbd9edbd6313b34f960a6f6f35384ece02e389a4
Brian <XaNTo1337@gmail.com>
2019-10-31 11:14:55 +0100

    bah

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser'); // Parse all JSON in incoming request
 const morgan = require('morgan'); // Log out all http requests to the console
 const cors = require('cors');
 const mongoose = require('mongoose'); // We need the mongoose library
-const path = require('path');
+// const path = require('path');
 
 
 
