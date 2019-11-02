ff0ae93aff574c818ccca963806c495d60d45910
Brian <XaNTo1337@gmail.com>
2019-11-02 13:05:09 +0100

    Added data layer

---
+++ client/build/precache-manifest.66c85da9b9da9c42a049e7786b66d587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88096acaaf1ed9a2b89e184e272b111e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3900766feca3aa5c5e49",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a68f3447bf422d6d8eca",
+    "url": "/static/js/2.c597d4e5.chunk.js"
+  },
+  {
+    "revision": "3900766feca3aa5c5e49",
+    "url": "/static/js/main.0cc00dd8.chunk.js"
+  },
+  {
+    "revision": "3420f20996158c56a481",
+    "url": "/static/js/runtime-main.3bca8455.js"
+  }
+]);
