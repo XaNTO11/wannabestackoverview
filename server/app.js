d07ce0cc354577815c6b32dcce8a8796a9418a02
Brian <XaNTo1337@gmail.com>
2019-11-02 14:40:27 +0100

    Shoot

--- server/app.js
+++ server/app.js
@@ -69,9 +69,9 @@ app.get(`/api/question/:id`, async (req, res) => {
         question
     })
 });
-app.get(`/api/question/:id/answer/`, async (req, res) => {
+app.get(`/api/question/answer/:id`, async (req, res) => {
     const _id = req.params.id;
-    let answers = await Question.findById(_id).select('answers').where('_id' == "5dbd7d7dfbaa55001777af43" );
+    let answers = await Question.select('answers').findById(_id).select();
     console.log(Question.findById(_id).title)
     return res.status(202).send({
         error: false,
