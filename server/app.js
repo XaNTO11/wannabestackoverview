b4b039ff835c0e2db9bccb2de46076d45e4d4b73
Brian <XaNTo1337@gmail.com>
2019-11-05 14:30:16 +0100

    hey

--- server/app.js
+++ server/app.js
@@ -119,7 +119,7 @@ app.put(`/api/question/:id`, async (req, res) => {
     })
 
 });
-app.get(`/api/question/answers/:id`, async (req, res) => {
+app.put(`/api/question/answers/:id`, async (req, res) => {
     const _id = req.params.id;
     // await Question.findOne({'answers._id': _id}, function (err, answer){
     //     if (err) return handleError(err);
@@ -131,24 +131,16 @@ app.get(`/api/question/answers/:id`, async (req, res) => {
     // } );
     // let answers = await Question.find({'answers._id':{_id}});
 
-    let answer = await Question.find({"answers": {_id: _id}})
+    // let answer = await Question.find({"answers": {_id: _id}})
     // answer.update({"answers._id": _id}, {$inc:{"votes": 1}})
-    // let answer = await Question.findOneAndUpdate({'answers._id': _id},
-    //     {
-    //     $inc: { "votes": 1}
-    //     },
-    //     function (err){
-    //     if (err) return handleError(err);
-    //     });
+    let answer = await Question.findOneAndUpdate({'answers._id': _id},
+        {
+        $inc: { 'answers.votes': 1}
+        }
     // answer.save()
         // console.log(person.authorName)
-
     // console.log(Question.findById(_id).title)
-    return res.status(202).send({
-        error:false,
-        answer
-    })
-
+    return res.status(200).send(answer)
 });
 //     app.delete(`/api/question/:id`, async (req, res) => {
 //         const {id} = req.params;
